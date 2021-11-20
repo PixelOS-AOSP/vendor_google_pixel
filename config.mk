b1da88f86505ae570fc465b64c3b01d5179215b8
AdarshGrewal <adarshgrewal@gmail.com>
2021-11-20 18:38:04 +0530

    pixel: Import SetupWizard props
    * taken from Pixel 6 Pro
    
    Change-Id: If2eebc804a2637f974449c67e70bbe4576ced0e5

--- config.mk
+++ config.mk
@@ -44,6 +44,19 @@ PRODUCT_PACKAGES += \
     com.google.android.apps.dialer.call_recording_audio.features \
     product_charger_res_images
 
+# SetupWizard
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.enterprise_mode=1 \
+    ro.setupwizard.esim_cid_ignore=00000001 \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.feature.baseline_setupwizard_enabled=true \
+    setupwizard.feature.day_night_mode_enabled=true \
+    setupwizard.feature.portal_notification=true \
+    setupwizard.feature.show_pai_screen_in_main_flow.carrier1839=false \
+    setupwizard.feature.show_pixel_tos=true \
+    setupwizard.feature.show_support_link_in_deferred_setup=false \
+    setupwizard.feature.skip_button_use_mobile_data.carrier1839=true
+
 # Set default ringtone, notification and alarm
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.config.alarm_alert=Fresh_start.ogg \
